f8851a88459fcacadb55353a9801c1b9c5d72b27
fledo97 <matteo.delforno97.md@gmail.com>
2026-03-05 14:23:28 +0100

    Update app.js

--- app.js
+++ app.js
@@ -216,7 +216,62 @@ if(ctx){
   });
 
 }
+// PIE CHART CATEGORIE ANNO
 
+let categoryTotals = {};
+
+transactions.forEach(t=>{
+
+  if(t.type !== "expense") return;
+  if(!t.topic) return;
+
+  const year = new Date(t.date).getFullYear();
+  const currentYear = new Date().getFullYear();
+
+  if(year !== currentYear) return;
+
+  if(!categoryTotals[t.topic]){
+    categoryTotals[t.topic] = 0;
+  }
+
+  categoryTotals[t.topic] += Number(t.amount);
+
+});
+
+const categoryLabels = Object.keys(categoryTotals);
+const categoryValues = Object.values(categoryTotals);
+
+const ctxPie = document.getElementById("categoryChart");
+
+if(ctxPie && categoryLabels.length > 0){
+
+  new Chart(ctxPie,{
+    type:"pie",
+    data:{
+      labels:categoryLabels,
+      datasets:[{
+        data:categoryValues
+      }]
+    },
+    options:{
+      plugins:{
+        tooltip:{
+          callbacks:{
+            label:function(context){
+
+              const total = categoryValues.reduce((a,b)=>a+b,0);
+              const value = context.raw;
+              const percent = ((value/total)*100).toFixed(1);
+
+              return `${context.label}: €${value} (${percent}%)`;
+            }
+          }
+        }
+      }
+    }
+  });
+// MOSTRA DATA
+}
 function showCurrentDate(){
 
   const now = new Date();
@@ -251,5 +306,6 @@ if("serviceWorker" in navigator){
 
   navigator.serviceWorker.register("service-worker.js")
     .then(() => console.log("Service Worker attivo"));
+
 
 }
